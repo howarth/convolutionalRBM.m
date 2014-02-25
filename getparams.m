4e6f1957395f504ef85ecd4d339519172559bc2f
howarth <d.c.howarth@gmail.com>
2014-02-25 16:41:04 -0500

    Added comments to getparams

--- getparams.m
+++ getparams.m
@@ -6,10 +6,10 @@ function params = getparams(method)
 %   Written by: Peng Qi, Sep 27, 2012
 
 %% Model parameters
-params.nmap = 16;
-params.szFilter = 8;
-params.szPool = 2;
-params.method = 'CD';
+params.nmap = 16; % number of filters
+params.szFilter = 8; % size of square filter
+params.szPool = 2; % size of square max pool filter
+params.method = 'CD'; % learning method
 
 if (nargin > 0)
     if strcmp(method, 'PCD'),
@@ -18,23 +18,23 @@ if (nargin > 0)
 end
 
 %% Learining parameters
-params.epshbias = 1e-1;
-params.epsvbias = 1e-1;
-params.epsW = 1e-2;
-params.phbias = 0.5;
-params.pvbias = 0.5;
-params.pW = 0.5;
-params.decayw = .01;
-params.szBatch = 10;
-params.sparseness = .02;
-params.whitenData = 1;
+params.epshbias = 1e-1; % learning rate for hidden bias
+params.epsvbias = 1e-1; % learning rate for visible bias
+params.epsW = 1e-2; % learning rate for weights
+params.phbias = 0.5; % momentum of hidden bias
+params.pvbias = 0.5; % momentum of visual bias
+params.pW = 0.5; % momentum of W
+params.decayw = .01; % exponential decay value
+params.szBatch = 10; % size of training batch
+params.sparseness = .02; % sparseness of hidden nodes
+params.whitenData = 1; % 1 if you want to whiten data, 0 if not
 
 %% Running parameters
-params.iter = 10000;
-params.verbose = 2;
-params.mfIter = 5;
-params.saveInterv = 5;
-params.useCuda = 0;
+params.iter = 10000; % number of learning iterations
+params.verbose = 2; % verbosity level
+params.mfIter = 5; % ?
+params.saveInterv = 5; 
+params.useCuda = 0; 
 params.saveName = 'model.mat';
 
 end
